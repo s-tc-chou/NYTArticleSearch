7d7c7d688c6c0e7792b3425e918e6b4f150e1bd4
s-tc-chou <steve.tc.chou@gmail.com>
2016-07-30 10:11:00 -0500

    Setting menu implementation (SC 7/30/2016)

--- app/src/main/java/com/example/steve/nytarticlesearch/Activities/editOptionFragment.java
+++ app/src/main/java/com/example/steve/nytarticlesearch/Activities/editOptionFragment.java
@@ -2,17 +2,20 @@ package com.example.steve.nytarticlesearch.Activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 
+import com.example.steve.nytarticlesearch.Models.editOptions;
 import com.example.steve.nytarticlesearch.R;
 import com.example.steve.nytarticlesearch.databinding.FragmentEditOptionBinding;
 
@@ -23,20 +26,14 @@ import java.util.Calendar;
 
 public class editOptionFragment extends DialogFragment {
 
-//    private OnFragmentInteractionListener mListener;
+    //    private OnFragmentInteractionListener mListener;
     private FragmentEditOptionBinding binding;
+    private editOptions settings;
 
     // Required empty public constructor
-    public editOptionFragment() {}
-
-    public static editOptionFragment newInstance(String param1, String param2) {
-        editOptionFragment fragment = new editOptionFragment();
-        //Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
-        //args.putString(ARG_PARAM2, param2);
-        //fragment.setArguments(args);
-        return fragment;
+    public editOptionFragment() {
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,13 +43,8 @@ public class editOptionFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit_option, container, false);
         View view = binding.getRoot();
-
-
-        // Inflate the layout for this fragment
-        //initBtnSaveOnClickListener();
 
         return view;
     }
@@ -61,37 +53,138 @@ public class editOptionFragment extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        initializeData();
+        initBtnSaveOnClickListener();
+        initBtnCancelOnClickListener();
     }
 
-    public void initBtnSaveOnClickListener(View v)
-    {
+    //Initialize all setting datas, read from search activity to keep state.
+    public void initializeData() {
+        SearchActivity myActivity = (SearchActivity) getActivity();
+        settings = myActivity.getCurrentSettings();
 
-    }
+        //set all the fields.
+        //check if null?
 
-    public void checkRadioButtonState()
-    {
+        //variables from settings
+        String filterText = settings.getFilter();
+        String sortOrder = settings.getSortOrder();
+        int year = settings.getYear();
+        int month = settings.getMonth();
+        int day = settings.getDay();
+        Boolean useBeginDate = settings.getUseBeginDate();
 
-    }
 
-    /*    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        //set filtertext, check for null.
+        if (filterText == null) {
+            binding.etFilterText.setText("");
+        } else {
+            binding.etFilterText.setText(filterText);
         }
-    }*/
 
-    //initialize date picker dialog and set to current date
-    /*public void initDatePickerDialog(View v)
+        //set sort order radio button.
+        if (sortOrder.equals(getString(R.string.oldest))) {
+            binding.rbOldest.setChecked(true);
+            binding.rbNewest.setChecked(false);
+        } else {
+            binding.rbOldest.setChecked(false);
+            binding.rbNewest.setChecked(true);
+        }
+
+        //set date picker for begin date & checkbox
+        binding.dpBeginDate.updateDate(year, month, day);
+        binding.cbUseBeginDate.setChecked(useBeginDate);
+
+    }
+
+    public void initBtnSaveOnClickListener() {
+
+        //initialize object.
+        settings = new editOptions();
+
+        binding.btnSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filter = binding.etFilterText.getText().toString();
+                if (filter.equals(null) || filter.equals(""))
+                {
+                    filterAlert();
+                }
+                else {
+                    //save all of our options
+                    editOptions currentOption = getEverything();
+                    onEditFinishedListener listener = (onEditFinishedListener) getActivity();
+                    listener.onEditFinish(currentOption);
+                    dismiss();
+                }
+
+            }
+        });
+    }
+
+
+    public void initBtnCancelOnClickListener() {
+        binding.btnCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    //Helper functions -------------------------
+
+    //
+    public void filterAlert()
     {
-        DatePicker dateFilter = (DatePicker)v.findViewById(R.id.datePicker);
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
+        builder1.setMessage("Filter cannot be blank!");
+        builder1.setCancelable(true);
 
-        Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        builder1.setNeutralButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
 
-        dateFilter.init(year,month,day,null);
-    }*/
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
 
+    //set all our variables in the settings menu
+    private editOptions getEverything()
+    {
+
+        String filterText = binding.etFilterText.getText().toString();
+
+        //set sortOrder based on radio button
+        String sortOrder;
+        if (binding.rbNewest.isChecked())
+        {
+            sortOrder = getString(R.string.newest);
+        }
+        else
+        {
+            sortOrder = getString(R.string.oldest);
+        }
+
+        int year = binding.dpBeginDate.getYear();
+        int month = binding.dpBeginDate.getMonth();
+        int day = binding.dpBeginDate.getDayOfMonth();
+        Boolean useBeginDate = binding.cbUseBeginDate.isChecked();
+
+        //instantiate the constructor with params we used.
+        editOptions currentSettings = new editOptions(filterText,sortOrder,year,month,day,useBeginDate);
+
+        return currentSettings;
+    }
+
+    //interface for killing fragment.
+    public interface onEditFinishedListener
+    {
+        void onEditFinish(editOptions editedSettings);
+    }
 
 }
--- app/src/main/java/com/example/steve/nytarticlesearch/Models/editOptions.java
+++ app/src/main/java/com/example/steve/nytarticlesearch/Models/editOptions.java
@@ -1,7 +1,91 @@
 package com.example.steve.nytarticlesearch.Models;
 
-/**
- * Created by Steve on 7/29/2016.
- */
-public class editOptions {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Calendar;
+
+
+public class editOptions{
+
+    private String filter;
+    private String sortOrder;//newest or oldest
+    private Boolean useBeginDate;
+    private int year;
+    private int month;
+    private int day;
+
+    public editOptions() {
+        filter = "";
+        sortOrder = "newest";
+
+        //set calendar date to today
+        Calendar date = Calendar.getInstance();
+        year = date.get(Calendar.YEAR);
+        month = date.get(Calendar.MONTH);
+        day = date.get(Calendar.DAY_OF_MONTH);
+        useBeginDate = false;
+
+    }
+
+    public editOptions(String filter, String sortOrder, int year, int month, int day, Boolean useBeginDate)
+    {
+        this.filter = filter;
+        this.sortOrder = sortOrder;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.useBeginDate = useBeginDate;
+
+    }
+
+    //generated getter
+    public String getFilter() {
+        return filter;
+    }
+
+    public String getSortOrder() {
+        return sortOrder;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public Boolean getUseBeginDate() {
+        return useBeginDate;
+    }
+
+    //generated setter
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public void setSortOrder(String sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setUseBeginDate(Boolean useBeginDate) {
+        this.useBeginDate = useBeginDate;
+    }
 }
